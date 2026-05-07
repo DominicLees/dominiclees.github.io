17e3bbff77a428ef04a8d71e458a89fa23a6afc5
Dominic Lees <leesdominic@gmail.com>
2026-05-07 11:22:58 +0100

    Added Sass compiling to build.js

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 const pug = require('pug');
+const sass = require('sass');
 const fs = require('fs');
 const path = require('path');
 
 if (!fs.existsSync('public')) fs.mkdirSync('public');
 
-const files = fs.readdirSync('./templates', { recursive: true }).filter(file => file.endsWith('.pug'));
-files.forEach(file => {
+// Compile pug files into html
+const pugFiles = fs.readdirSync('./templates', { recursive: true }).filter(file => file.endsWith('.pug'));
+pugFiles.forEach(file => {
     const inputPath = path.join('./templates', file);
     const outputPath = path.join('./public', file.replace('.pug', '.html'));
 
@@ -13,4 +15,14 @@ files.forEach(file => {
 
     const html = pug.renderFile(inputPath);
     fs.writeFileSync(outputPath, html);
+})
+
+// Compile SCSS files into CSS
+const scssFiles = fs.readdirSync('./src/styles', { recursive: true }).filter(file => file.endsWith('.scss'));
+scssFiles.forEach(file => {
+    const inputPath = path.join('./src/styles', file);
+    const outputPath = path.join('./public/styles', file.replace('.scss', '.css'));
+    fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+    const css = sass.compile(inputPath, {style: "compressed"}).css;
+    fs.writeFileSync(outputPath, css);
 })
