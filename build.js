ff5f604b1910f1dd875e4a1f830816f8b0a340f4
Dominic Lees <leesdominic@gmail.com>
2026-05-07 13:21:08 +0100

    Updated index page to include most recent blog posts

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ const sass = require('sass');
 const fs = require('fs');
 const path = require('path');
 
+let articles = [];
+
 // Compile complete pug files into html
 async function compilePages() {
     console.log("Starting compiling pages files");
@@ -13,26 +15,35 @@ async function compilePages() {
 
         fs.mkdirSync(path.dirname(outputPath), { recursive: true });
 
-        const html = pug.renderFile(inputPath);
+        const html = pug.renderFile(inputPath, {articles});
         fs.writeFileSync(outputPath, html);
     })
     console.log("Finished compiling pages files");
 }
 
 // Compile blog posts
-async function compileBlogPosts() {
+function compileBlogPosts() {
     console.log("Starting compiling blog posts");
     const postTemplate = pug.compileFile('./src/templates/post.pug');
     const pugFiles = fs.readdirSync('./posts', { recursive: true }).filter(file => file.endsWith('.pug'));
     pugFiles.forEach(file => {
         const inputPath = path.join('./posts', file);
-        const outputPath = path.join('./public/post', file.replace('.pug', '.html'));
+        const newFileName = file.replace('.pug', '.html')
+        const outputPath = path.join('./public/post', newFileName);
 
         fs.mkdirSync(path.dirname(outputPath), { recursive: true });
 
         const article = pug.renderFile(inputPath);
         const html = postTemplate({article})
         fs.writeFileSync(outputPath, html);
+        
+        // cache rendered articles
+        if (articles.length >= 10) return;
+        articles.push({
+            filename: newFileName,
+            title: article.split('<h1>').pop().split('</h1>')[0],
+            subtitle: article.split('<h2>').pop().split('</h2>')[0]
+        })
     })
     console.log("Finished compiling blog posts");
 }
@@ -51,7 +62,8 @@ async function compileCSS() {
     console.log("Finished compiling SCSS files");
 }
 
-Promise.all([compilePages(), compileCSS(), compileBlogPosts()])
+compileBlogPosts()
+Promise.all([compilePages(), compileCSS()])
 .then(() => {
     console.log("All tasks complete");
 }).catch(error => {
