2fd881211064767e59168cd139ba59bac588058e
Dominic Lees <leesdominic@gmail.com>
2026-05-07 10:56:09 +0100

    Updated build.js to handle multiple pug files

--- build.js
+++ build.js
@@ -1,6 +1,16 @@
 const pug = require('pug');
 const fs = require('fs');
+const path = require('path');
 
-const html = pug.renderFile('./templates/index.pug');
-fs.mkdirSync('public')
-fs.writeFileSync('./public/index.html', html, {});
+if (!fs.existsSync('public')) fs.mkdirSync('public');
+
+const files = fs.readdirSync('./templates', { recursive: true }).filter(file => file.endsWith('.pug'));
+files.forEach(file => {
+    const inputPath = path.join('./templates', file);
+    const outputPath = path.join('./public', file.replace('.pug', '.html'));
+
+    fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+
+    const html = pug.renderFile(inputPath);
+    fs.writeFileSync(outputPath, html);
+})
